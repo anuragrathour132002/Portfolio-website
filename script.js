b890e377f8506a71a88e943393ca317197810d5e
Anurag Rathour <83803557+anuragrathour132002@users.noreply.github.com>
2022-10-30 14:07:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Developer", "Designer","Comptative Coder"],
+        strings: ["Developer", "Designer","Competitive Coder"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings:  ["Developer",  "Designer", "Compatative Coder"],
+        strings:  ["Developer",  "Designer", "Competitive Coder"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
